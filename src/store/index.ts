a9dd2259106eef60df967e3790c089ee8257058b
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2025-03-16 17:12:41 -0600

    Milestone 7

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
 import { createStore } from "vuex"
 import { auth } from "./auth.module"
-
+import { user } from "./user.module"
 const store = createStore({
 	modules: {
-		auth
+		auth,
+		user
 	}
 })
 
-// There must be an export in order to import it
 export default store
